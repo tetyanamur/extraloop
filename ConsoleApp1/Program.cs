9fbf73d669d5dbc26f286566e3ae3e84356adf5f
Tetyana Murzina <tetyanamur@gmail.com>
2022-08-10 18:11:40 -0500

    initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+
+            bool IsMonth;
+          
+            {
+
+
+                do 
+                {
+                    ;
+                    Console.WriteLine("Independence month?");
+
+                   string sample = Console.ReadLine();
+
+
+            
+                  
+
+
+
+                
+                    if  (sample == "july")
+                    {
+                     Console.WriteLine("Correct");
+                    }
+                    
+                    else
+                    {
+                        Console.WriteLine("Wrong");
+                    }
+                    // Do Something
+                   
+                    IsMonth = Boolean.TryParse(sample, out IsMonth);
+
+                } while (IsMonth == false) ;
+                }
+                }
+        }
+    }
+
+
+
+
+
+                    
+
+                    
